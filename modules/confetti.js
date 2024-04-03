a3503e656d1fff96217f10172e94f73924e47dc6
Shcoobz <a.boeckmann23@gmail.com>
2024-04-03 13:35:58 +0200

    refactored code: added modules

--- modules/confetti.js
+++ modules/confetti.js
@@ -1,7 +1,7 @@
 // Confetti.js - downloaded from https://www.cssscript.com/css-css3/
 
-var maxParticleCount = 150; //set max confetti count
-var particleSpeed = 2; //set the particle animation speed
+var maxParticleCount = 100; //set max confetti count
+var particleSpeed = 5; //set the particle animation speed
 var startConfetti; //call to start confetti animation
 var stopConfetti; //call to stop adding confetti
 var toggleConfetti; //call to start or stop the confetti animation depending on whether it's already running
@@ -60,10 +60,14 @@ function startConfettiInner() {
   if (canvas === null) {
     canvas = document.createElement('canvas');
     canvas.setAttribute('id', 'confetti-canvas');
-    canvas.setAttribute('style', 'display:block;z-index:999999;pointer-events:none');
+    canvas.setAttribute(
+      'style',
+      'position:absolute;top:0;left:0;display:block;z-index:999999;pointer-events:none'
+    );
     document.body.appendChild(canvas);
     canvas.width = width;
     canvas.height = height;
+    document.body.style.margin = '0';
     window.addEventListener(
       'resize',
       function () {
@@ -143,3 +147,5 @@ function updateParticles() {
     }
   }
 }
+
+export { startConfetti, stopConfetti, removeConfetti };
